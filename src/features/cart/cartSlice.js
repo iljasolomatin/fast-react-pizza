058161a66fea2263f52ad2734f34f1c6de3c88f4
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-06-30 14:01:32 +0100

    Integrate Redux state and clear cart in Cart feature
    Replaced fake cart data with Redux state in Cart.jsx, added clear cart functionality using a new clearCart action, and improved EmptyCart to use LinkButton and updated styling. Also added selectors for cart and username in cartSlice.js.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -49,9 +49,13 @@ export const {
   deleteItem,
   increaseItemQuantity,
   decreaseItemQuantity,
+  clearCart,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+export const getUsername = (state) => state.user.username;
 
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
